9e7c1ce0e6a21d0f271c3e5ed9f96d6fce06e443
Matheus Viscki <matheusviscki@gmail.com>
2025-01-23 08:29:23 -0300

    feat: habilitar o "acesso" ao estilo do componente genérico

--- src/app/components/shared/field-control/field-control.component.ts
+++ src/app/components/shared/field-control/field-control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-field-control',
   standalone: true,
   imports: [],
   templateUrl: './field-control.component.html',
-  styleUrl: './field-control.component.css'
+  styleUrl: './field-control.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class FieldControlComponent {
   title = input.required();
