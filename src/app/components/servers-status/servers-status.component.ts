1fe1e8490734e2707e8622bca9246434ba9fb77c
Matheus Viscki <matheusviscki@gmail.com>
2025-01-27 06:53:35 -0300

    feat: implementar método do lifecycle angular

--- src/app/components/servers-status/servers-status.component.ts
+++ src/app/components/servers-status/servers-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers-status',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './servers-status.component.html',
   styleUrl: './servers-status.component.css'
 })
-export class ServersStatusComponent {
+export class ServersStatusComponent implements OnInit, OnDestroy {
   currentStatus = 'online';
+  private interval?: ReturnType<typeof setInterval>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.interval = setInterval(() => {
+      const random = Math.random();
+
+      if (random < 0.5) {
+        return this.currentStatus = 'online';
+      } else if (random < 0.9) { 
+        return this.currentStatus = 'offline';
+      } else {
+        return this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
