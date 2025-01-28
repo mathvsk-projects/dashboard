09aea00e069bf086074eb3869b63d0a22afec79a
Matheus Viscki <matheusviscki@gmail.com>
2025-01-28 07:51:57 -0300

    feat: alterar para two way binding

--- src/app/components/new-ticket/new-ticket.component.ts
+++ src/app/components/new-ticket/new-ticket.component.ts
@@ -13,8 +13,14 @@ import { FieldControlComponent } from "../shared/field-control/field-control.com
 export class NewTicketComponent {
   inputValue = viewChild<HTMLInputElement>('titleInput');
   add = output<{title: string, text: string}>();
+  
+  title = "";
+  request = "";
 
-  onSubmit(title: string, ticketText: string) {
-    this.add.emit({title: title, text: ticketText});
+  onSubmit() {
+    this.add.emit({
+      title: this.title,
+      text: this.request
+    });
   }
 }
